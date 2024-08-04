e9d6a80a868d267c98db6341a0c8358002a5c8cb
aleks <aleksejserakov45@gmail.com>
2024-08-04 20:36:17 +0700

    modified:   public/index.html 	modified:   public/style.css 	modified:   server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { futimesSync } = require('fs');
 const http = require('http');
 const path = require('path');
 const socketIo = require('socket.io');
@@ -7,6 +8,10 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 const rooms = {};
+const questions = [
+  {question: 'Вопрос 1', answers: ['vfibyf', 'cavjktn', 'hfrtnf', 'nfyr'], correct: 0},
+  {question: 'Вопрос 2', answers: ['felb', 'naqjnf', 'vthctltc', ',vd'], correct: 2}
+];
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,7 +20,7 @@ io.on('connection', (socket) => {
 
   socket.on('create-room', () => {
     const roomId = Math.random().toString(36).substr(2, 5);
-    rooms[roomId] = { players: [], creator: socket.id };
+    rooms[roomId] = { players: [], creator: socket.id, currentQuestion: 0, answers: {} };
     socket.join(roomId);
     socket.emit('room-created', roomId);
     console.log(`Room ${roomId} created`);
@@ -24,8 +29,9 @@ io.on('connection', (socket) => {
   socket.on('join-room', (roomId, nickname) => {
     if (rooms[roomId]) {
       const imageIndex = rooms[roomId].players.length % 5;
-      const user = { id: socket.id, nickname, ready: false, image: `file${imageIndex + 1}.png` };
+      const user = { id: socket.id, nickname, ready: false, image: `file${imageIndex + 1}.png`, score: 0 };
       rooms[roomId].players.push(user);
+      user.score = 0;
       socket.join(roomId);
       socket.emit('room-joined', roomId, rooms[roomId].players);
       io.to(roomId).emit('user-joined', rooms[roomId].players);
@@ -45,7 +51,8 @@ io.on('connection', (socket) => {
         io.to(roomId).emit('user-joined', room.players);
         console.log(`Player ${nickname} is ready in room ${roomId}`);
         if (room.players.every(p => p.ready)) {
-          io.to(roomId).emit('start-game');
+          //io.to(roomId).emit('start-game');
+          StartNextQuestion(roomId);
         }
       }
     }
@@ -75,7 +82,54 @@ io.on('connection', (socket) => {
       }
     }
   });
+
+  socket.on('answer', (roomId, nickname, answerIndex) => {
+    const room = rooms[roomId];
+    if(room){
+      const player = room.players.find(p => p.nickname === nickname);
+      if (player) {
+      if(questions[room.currentQuestion--].correct === answerIndex) {
+        console.log('Correct answer'); 
+         
+        player.score+=50;
+        console.log('your score is '+ player.score);
+          
+      }
+      else{
+        console.log('Incrorect answer, your answer is '+ answerIndex);
+        console.log('correct answer is ' + questions[room.currentQuestion].correct)
+        console.log('your score is '+ player.score);
+      }
+      room.answers[nickname] = answerIndex;
+      if (Object.keys(room.answers).length === room.players.length) {
+        startNextQuestion(roomId);
+      }
+    }
+  }})
 });
+
+  function StartNextQuestion(roomid){
+    const room = rooms[roomid];
+    if(room){
+      const questionIndex = room.currentQuestion;
+      if(questionIndex < questions.length){
+        io.to(roomid).emit('next-question', questions[questionIndex]);
+        room.currentQuestion++;
+        room.answers = {};
+        setTimeout(() => {
+          
+            StartNextQuestion(roomid);
+          
+          
+        }, 30000);
+      }
+      else{
+        io.to(roomid).emit('quiz-ended');
+      }
+    }
+  }
+  
+  
 
 server.listen(3000, () => {
   console.log('Server running at http://localhost:3000');
